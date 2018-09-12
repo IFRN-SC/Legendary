2b44838c0199bb8a563e1778b4898bb4e99c928e
duduCMT <startonze@gmail.com>
2018-09-12 20:28:24 -0300

    Reconstrução do Seguir Linha
    Todas as estrategias do Seguir Linha foram removidas para recomeçar a construção do código com novas estrategias, algumas funcionalidades basicas foram mantidas para tornar essa versão "genérica".

--- LEGENDARY_CODIGO2.0/Estrategia.cpp
+++ LEGENDARY_CODIGO2.0/Estrategia.cpp
@@ -1,9 +1,6 @@
 #include "Estrategia.h"
-#define DELAY_PONTA 150
-#define DELAY_LADO 90
-#define DELAY_FESTA 100
-#define DELAY_EXPULSANDO_DA_FESTA 50 
-#define DELAY_CONTINUE 90
+#define DISTANCIA 4.60
+#define DELAY_TAMANHO_OBSTACULO 1200
 
 Estrategia::Estrategia(){
   //
@@ -25,77 +22,33 @@ void Estrategia::seguirLinha(){
     frente = false;
     direita = false;
     esquerda = true;
-    
   }else if(sensores.bbpp()){
-    if(!sensores.bbpp()){
-      while(sensores.bbpb()){
-        motores.girarDir();
-      }
-    } else {
-      while(sensores.bbpb()){
-        motores.girarDir();
-        delay(30);
-      }
-      while(sensores.bbbb()){
-        motores.girarDir();
-        delay(30);
-      }
-      
-      while(sensores.bbpb()){
-        motores.girarEsq();
-      }
-    }
+    motores.girarDir();
     frente = false;
     direita = true;
     esquerda = false;
-    
-  }else if(sensores.ppbb()){    
-    if(!sensores.ppbb()){
-      while(sensores.bpbb()){
-        motores.virarEsq();
-      }
-    } else {
-      while(sensores.bpbb()){
-        motores.girarEsq();
-        delay(50);
-      }
-      while(sensores.bbbb()){
-        motores.girarEsq();
-        delay(50);
-      }
-      while(sensores.bpbb()){
-        motores.girarDir();
-      }
-    }
+  }else if(sensores.ppbb()){
+    motores.girarEsq();
     frente = false;
     direita = false;
     esquerda = true;
-
   }else if(sensores.bbbp()){
-    motores.voltarDir();
-    delay(90);   
     motores.girarDir();
-    delay(DELAY_PONTA);
     frente = false;
     direita = true;
     esquerda = false;
   }else if(sensores.pbbb()){
-    motores.voltarEsq();
-    delay(90);
     motores.girarEsq();
-    delay(DELAY_PONTA);
     frente = false;
     direita = false;
     esquerda = true;
   }else if(sensores.bppp()){
-    motores.virarDir();
-    delay(DELAY_EXPULSANDO_DA_FESTA);
+    motores.girarDir();
     frente = false;
     direita = true;
     esquerda = false;
   }else if(sensores.pppb()){
-    motores.virarEsq();
-    delay(DELAY_EXPULSANDO_DA_FESTA);
+    motores.girarEsq();
     frente = false;
     direita = false;
     esquerda = true;
@@ -105,16 +58,43 @@ void Estrategia::seguirLinha(){
     direita = false;
     esquerda = false;
   }else if(sensores.bppb()){
-    if(frente){
-      motores.frente();
-    }else if(direita){
-      motores.girarDir();
-    }else if(esquerda){
-      motores.girarEsq();
-    }
-    delay(DELAY_CONTINUE);
+    //Seja Criativo 
   }else{
     //Ação Anterior
+  }
+}
+
+void Estrategia::desviarObstaculo(){
+  if(sensores.sonar()){
+    motores.parar();
+    delay(1000);
+    if(sensores.sonar()){
+      motores.girar90Dir();
+      motores.parar();
+      delay(1000);
+      motores.frente();
+      delay(750);
+      motores.parar();
+      delay(1000);
+      motores.girar90Esq();
+      motores.parar();
+      delay(1000);
+      motores.frente();
+      delay(DELAY_TAMANHO_OBSTACULO);
+      motores.parar();
+      delay(1000);
+      motores.girar90Esq();
+      motores.parar();
+      delay(1000);
+      while(!sensores.bbbb()){
+        motores.frente(); 
+      }
+      motores.parar();
+      delay(1000);
+      motores.girar90Dir();
+      motores.parar();
+      delay(1000);
+    } 
   }
 }
 
@@ -126,7 +106,11 @@ void Estrategia::start(){
 }
 
 void Estrategia::executar(){
+  robo.desligarLed(1);
+  robo.desligarLed(2);
+  robo.desligarLed(3);
   start();
+  //desviarObstaculo();
   seguirLinha();
 }
 
